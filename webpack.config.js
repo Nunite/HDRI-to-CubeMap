4a6c416470dc58c668481e96f28b7f3ed4def33e
Nunite <1318205957@qq.com>
2025-04-14 21:39:25 +0800

    配置master分支直接部署GitHub Pages到docs目录

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = () => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'docs'),
       filename: 'bundle.js'
     },
     module: {
@@ -19,7 +19,7 @@ module.exports = () => {
       ]
     },
     devServer: {
-      contentBase: path.join(__dirname, 'public'),
+      contentBase: path.join(__dirname, 'docs'),
       historyApiFallback: true
     }
   }
